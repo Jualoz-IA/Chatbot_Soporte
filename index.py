c31815356203a6bce395a8a2c4e45a893f26c0ab
Juan Lozada <lozadagomezj@gmail.com>
2025-01-30 22:17:09 -0500

    Funcionamiento del index

--- index.py
+++ index.py
@@ -1,15 +1,14 @@
 import streamlit as st
 
 # Crear páginas
-""" TODO: Hacer el tema de login, que se muestren ciertas  """
-
-chat_page = st.Page("components/chat.py", title="ChatBot", icon=":material/smart_toy:")
-colletions_page = st.Page("components/collections.py", title="Collections", icon=":material/smart_toy:")
-doc_gestion_page = st.Page("components/doc-gestion.py", title="Documents Gestion", icon=":material/ar_on_you:")
-parameters_page = st.Page("components/parameters.py", title="Models Parameters", icon=":material/multiple_stop:")
-user_gestion_page = st.Page("components/user-gestion.py", title="User Gestion", icon=":material/ar_on_you:")
-
-""" Sera un modal """
-login_page = st.Page("components/login.py", title="Login", icon=":material/multiple_stop:") 
+chat_page = st.Page("components/chat/chat.py", title="ChatBot", icon=":material/smart_toy:")
+colletions_page = st.Page("components/collections/collections.py", title="Collections", icon=":material/smart_toy:")
+doc_gestion_page = st.Page("components/doc_gestion/doc-gestion.py", title="Documents Gestion", icon=":material/ar_on_you:")
+parameters_page = st.Page("components/parameters/parameters.py", title="Models Parameters", icon=":material/multiple_stop:")
+user_gestion_page = st.Page("components/user_gestion/user-gestion.py", title="User Gestion", icon=":material/ar_on_you:")
+login_page = st.Page("components/login/login.py", title="Login", icon=":material/multiple_stop:") 
 
 st.set_page_config(page_title="Chatbot", page_icon=":material/business_messages:")
+
+pg = st.navigation([chat_page, colletions_page, doc_gestion_page, parameters_page, user_gestion_page, login_page])
+pg.run()
