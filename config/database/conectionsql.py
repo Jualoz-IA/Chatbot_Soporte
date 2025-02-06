ac3c0903c0d4f16ee0e98b57a7262c7dfa0ae387
Juan Lozada <lozadagomezj@gmail.com>
2025-02-05 21:38:35 -0500

    Arreglo de errores - Funcionamiento del proyecto

--- config/database/conectionsql.py
+++ config/database/conectionsql.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./support.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Crear el motor de la base de datos
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine("postgresql+psycopg2://" + DATABASE_URL, isolation_level="REPEATABLE READ")
 
 # Crear la sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
