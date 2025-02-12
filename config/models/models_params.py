1372365c0697ed9721634928c27e85364c62bef6
Juan Lozada <lozadagomezj@gmail.com>
2025-02-12 17:17:38 -0500

    Ajuste float

--- config/models/models_params.py
+++ config/models/models_params.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String,Double
+from sqlalchemy import Column, Integer, String, Double, Float
 class ModelParams():
     __tablename__ = "model_config"
     
     id = Column(Integer, primary_key=True, index=True)
     currentGroqToken = Column(String)
     selected_model = Column(String)
-    temperature = Column(float)
+    temperature = Column(Float)
     max_tokens = Column(Integer)
     top_p = Column(Double)
     frequency_penalty = Column(Double)
